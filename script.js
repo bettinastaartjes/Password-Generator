de2f08f29dc3291bff8a8361201c51d9c6e398a3
Bettina Staartjes <bbstaars@gmail.com>
2022-08-22 17:41:55 -0400

    can click button and pop-up about length of password comes up

--- script.js
+++ script.js
@@ -4,6 +4,22 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword () {
+  var userInput = window.prompt ("Choose the length of your password.")
+  var passwordLength = parseInt (userInput)
+
+  if (isNaN(passwordLength)) {
+    window.alert("Thats not a number!")
+    return
+  }
+
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8-128 characters.")
+    return
+  }
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -15,3 +31,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+//// 1. We need to prompt the user for the password criteria
+  // a. Password length, between 8-128 characters
+  // b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input
+// 3. Geberate oasswird ivased ib critera
+// 4. Display the password to the page (return)
+
+//
